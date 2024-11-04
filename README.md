cff61d123294aedb876248f59c29c18c0137debf
ikbalhasan <ikbalhasan312@gmail.com>
2024-11-05 03:38:00 +0600

    digram added

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# exm
+## Create k3s cluster in private subnet and nginx load balancer in public subnet using terraform blueprint approach
+
+![alt diagram](./diagram.png)
+
--- environments/dev/bastion.tf
+++ environments/dev/bastion.tf
@@ -13,6 +13,9 @@ resource "aws_security_group" "bastion_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  tags = {
+    Name = "${var.environment}-bastion-sg"
+  }
 
 }
 
